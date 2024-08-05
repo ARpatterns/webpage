75777f5ef1a9f119581f9c697a971ea3d8f90319
Steven Haesler <hasv@zhaw.ch>
2024-08-05 14:04:21 +0200

    add differentiation for Github Pages

--- assets/js/util.js
+++ assets/js/util.js
@@ -585,9 +585,3 @@
 	};
 
 })(jQuery);
-
-
-
-const isGitHubPages = location.hostname.includes('github.io');
-const basePath = isGitHubPages ? '/webpage/' : '/';
-document.querySelector('base').setAttribute('href', basePath);
